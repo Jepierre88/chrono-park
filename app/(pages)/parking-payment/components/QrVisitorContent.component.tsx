8834658737f69dca5416aaabb45b828342f2e3a9
Jean Ortiz <jeanpi.22241@gmail.com>
2025-07-21 19:36:51 -0500

    Add Checkbox component and integrate into QrVisitorContent for service selection

--- app/(pages)/parking-payment/components/QrVisitorContent.component.tsx
+++ app/(pages)/parking-payment/components/QrVisitorContent.component.tsx
@@ -3,12 +3,19 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
 
 export const QrVisitorContent = ({ services }: { services: { id: string; name: string }[] }) => {
     const [selectedService, setSelectedService] = useState<string | undefined>();
-
+    const [payDay, setPayDay] = useState<boolean>(false);
     return (
-        <form>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+        }}>
+            <header>
+                <h2>Datos del visitante</h2>
+            </header>
             <Select value={selectedService} onValueChange={setSelectedService}>
                 <SelectTrigger className="w-[180px]">
                     <SelectValue placeholder="Seleccionar Servicio" />
@@ -23,6 +30,21 @@ export const QrVisitorContent = ({ services }: { services: { id: string; name: s
                     </SelectGroup>
                 </SelectContent>
             </Select>
+
+            <div className="flex items-center mt-4">
+                <Checkbox
+                    className="ml-0"
+                    name="payDay"
+                    checked={payDay}
+                    onCheckedChange={(e: boolean) => {
+                        setPayDay(e);
+                    }}
+                />
+                <Label className="ml-2" htmlFor="payDay">
+                    Dia pago
+                </Label>
+            </div>
+
 
             <input type="hidden" name="service" value={selectedService ?? ""} />
 
